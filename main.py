96d2a7dbdb4f2a2f2915f528d6436ac293ec96b1
Edoalfredo <getzbiealfredo@gmail.com>
2023-07-06 08:55:28 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from lib import*
+from model import*
+from preprocessing import*
+
+def main():
+    
+    st.title("Pinal Projek:blue[Review Ulasan(Sentiment Expression) dengan]:green[Python]:stuck_out_tongue_closed_eyes::smile::scream::blush::angry::rage:")
+    st.header('Multinomial Naive Bayes And Chi - Square')
+    teks = st.text_input('Input text')
+    hasil = preprocess_text(teks)
+    kalimat_normalisasi = normalized_term(hasil)
+
+    def convert_to_sentence(word_list):
+        sentence = ' '.join(word_list)
+        return sentence
+    kalimat = convert_to_sentence(kalimat_normalisasi)
+
+    if st.button('Prediksi Kalimat'):
+      predict_text(kalimat)
+
+if __name__ == '__main__':
+    main()
